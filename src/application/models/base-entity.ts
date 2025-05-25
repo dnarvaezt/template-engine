87fb5e68d8308a66ddfe562f90541058bfd04f27
David Narvaez <fnarvaez@ubits.co>
2025-05-24 20:48:10 -0500

    refactor: Remove obsolete application files and clean up codebase
    - Deleted unused application files including models, repositories, and services to streamline the codebase.
    - Removed BaseEntity, Repository, and WebRepository classes along with their associated exports for better maintainability.
    - Cleaned up the application structure by eliminating unnecessary code, enhancing overall clarity.

--- src/application/models/base-entity.ts
+++
@@ -1,8 +0,0 @@
-export class BaseEntity {
-  public id!: string
-
-  constructor(init: Partial<BaseEntity> = {}) {
-    init.id ??= crypto.randomUUID()
-    Object.assign(this, init)
-  }
-}
--- src/presentation/modules/generic-module/application/domain/ports/input/generic-module.create.use-case.ts
+++ src/presentation/modules/generic-module/application/domain/ports/input/generic-module.create.use-case.ts
@@ -2,9 +2,6 @@ import { GenericModule } from '../../entities/generic-module.entity'
 
 export interface CreateGenericModuleInput {
   name: string
-  description: string
-  price: number
-  stock: number
 }
 
 export interface CreateGenericModuleOutput extends GenericModule {}
